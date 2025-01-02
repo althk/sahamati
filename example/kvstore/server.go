aa8130e20868232a7af43274c825feeaf4f89731
Harish Kotha <althk@users.noreply.github.com>
2025-01-03 00:59:26 +0530

    add a simple HTTP server for the example kv store

---
+++ example/kvstore/server.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"io"
+	"log"
+	"net/http"
+)
+
+type HTTPServer struct {
+	addr     string
+	mntPoint string
+	store    *KVStore
+}
+
+func NewHTTPServer(addr, mntPoint string, store *KVStore) *HTTPServer {
+	return &HTTPServer{
+		addr:     addr,
+		mntPoint: mntPoint,
+		store:    store,
+	}
+}
+
+func (s *HTTPServer) Serve(ctx context.Context) error {
+	srv := &http.Server{
+		Addr:    s.addr,
+		Handler: s.Routes(),
+	}
+
+	shutdownCh := make(chan struct{})
+	go func(ctx context.Context) {
+		<-ctx.Done()
+		log.Println("Shutting down kvstore http server...")
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down kvstore http server: %v", err)
+			_ = srv.Close()
+		}
+		close(shutdownCh)
+	}(ctx)
+	log.Println("Starting kvstore http server...")
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	<-shutdownCh
+	return nil
+}
+
+func (s *HTTPServer) Routes() chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	r.Route(s.mntPoint, func(r chi.Router) {
+		r.Get("/{key}", s.Get)
+		r.Post("/", s.Post)
+		r.Put("/{key}", s.Put)
+	})
+	return r
+}
+
+func (s *HTTPServer) Get(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
+	v, ok := s.store.Get(key)
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	_, _ = w.Write([]byte(v))
+}
+
+func (s *HTTPServer) Put(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	_, ok := s.store.Get(key)
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	s.store.Put(key, string(value))
+}
+
+func (s *HTTPServer) Post(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	var e map[string]string
+	err = json.Unmarshal(data, &e)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.23
 require (
 	connectrpc.com/connect v1.17.0
 	github.com/althk/wal v0.0.0-20241130101942-adac4d0d00ae
+	github.com/go-chi/chi/v5 v5.2.0
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f
 	google.golang.org/protobuf v1.35.2
 )
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 )
